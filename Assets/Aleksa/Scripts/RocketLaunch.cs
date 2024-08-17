9a29374db7e25a2714692372053e7c95952c5dac
Aleksa Trampa <alektramp@gmail.com>
2024-08-17 22:21:44 +0200

    Added Stage 3 speed reduction over time

--- Assets/Aleksa/Scripts/RocketLaunch.cs
+++ Assets/Aleksa/Scripts/RocketLaunch.cs
@@ -6,79 +6,69 @@ using UnityEngine.Serialization;
 public class RocketLaunch : MonoBehaviour
 {
     public RocketStateMachine rocketStateMachine;
+    public RocketStageEvents rocketStageEvents;
+    
     public StageModel stage1;
     public StageModel stage2;
     public StageModel stage3;
+    
     public float maxSpeed = 100f;
     public float startAngle = 45f;  // Reference angle in degrees
+    public float gravityStrength = 9.81f;
     
-    private Vector2 position;
-    private Vector2 initialPosition = Vector2.zero;
-    private bool isLaunched = false;
+    private Vector2 _initialPosition = Vector2.zero;
 
     private void Awake()
     {
-        initialPosition = transform.position;
+        _initialPosition = transform.position;
         rocketStateMachine = GetComponent<RocketStateMachine>();
         rocketStateMachine.SetupStateMachine(this);
+        
+        rocketStageEvents = GetComponent<RocketStageEvents>();
 
-        stage1.OnStageUpdate = Stage1Update;
-        stage2.OnStageUpdate = Stage2Update;
+        stage1.OnStageStart = rocketStageEvents.Stage1Start;
+        stage2.OnStageStart = rocketStageEvents.Stage2Start;
+        stage3.OnStageStart = rocketStageEvents.Stage3Start;
+        stage1.OnStageUpdate = rocketStageEvents.Stage1Update;
+        stage2.OnStageUpdate = rocketStageEvents.Stage2Update;
+        stage3.OnStageUpdate = rocketStageEvents.Stage3Update;
+        stage1.OnStageEnd = rocketStageEvents.Stage1End;
+        stage2.OnStageEnd = rocketStageEvents.Stage2End;
+        stage3.OnStageEnd = rocketStageEvents.Stage3End;
     }
 
     public void Launch()
     {
-        position = initialPosition;
-        transform.position = initialPosition;
+        transform.position = _initialPosition;
 
         stage1.angleAtStageStart = startAngle;
         stage2.angleAtStageStart = startAngle;
+        stage3.angleAtStageStart = startAngle;
 
         rocketStateMachine.LaunchStateMachine();
-        isLaunched = true;
     }
 
     void Update()
     {
-        if (!isLaunched)
-            return;
-        
         rocketStateMachine.UpdateTime(Time.deltaTime); // handles stage start, update and end events
     }
 
-    public void Stage1Update(StageModel stage)
-    {
-        Vector2 stageLaunchDirection = GetFlightDirection(stage);
-        float speed = CalculateSpeed(stage);
-        
-        position += stageLaunchDirection * (speed * Time.deltaTime);
-        transform.position = position;
-    }
-    public void Stage2Update(StageModel stage)
-    {
-        Vector2 stageLaunchDirection = GetFlightDirection(stage);
-        float speed = CalculateSpeed(stage);
-        
-        position += stageLaunchDirection * (speed * Time.deltaTime);
-        transform.position = position;
-    }
+    
+    
+    
     public float CalculateStageDuration(StageModel stage)
     {
         float stageDuration = stage.GetStageDuration();
         return stageDuration;
     }
-    
     public float CalculateSpeed(StageModel stage)
     {
         float stageSpeed = stage.CalculateSpeed(maxSpeed);
         return stageSpeed;
     }
-    
     public Vector2 GetFlightDirection(StageModel stage)
     {
         Vector2 flightDirection = stage.GetFlightDirection();
         return flightDirection;
     }
-
-    public StageModel CurrentlyActiveStage => rocketStateMachine.CurrentActiveStage;
 }
--- Assets/Aleksa/Scripts/RocketStageEvents.cs
+++ Assets/Aleksa/Scripts/RocketStageEvents.cs
@@ -1,15 +1,21 @@
+using System;
 using UnityEngine;
 
 public class RocketStageEvents : MonoBehaviour
 {
     public RocketLaunch rocketLaunch;
     private Vector2 position;
+
+    private void Awake()
+    {
+        rocketLaunch = GetComponent<RocketLaunch>();
+    }
+    
     
     public void Stage1Start(StageModel stage)
     {
-        
+        position = Vector2.zero;
     }
-    
     public void Stage1Update(StageModel stage)
     {
         Vector2 stageLaunchDirection = rocketLaunch.GetFlightDirection(stage);
@@ -24,34 +30,64 @@ public class RocketStageEvents : MonoBehaviour
     }
     
     
+    
     public void Stage2Start(StageModel stage)
     {
         
     }
-
     public void Stage2Update(StageModel stage)
     {
+        Vector2 stageLaunchDirection = rocketLaunch.GetFlightDirection(stage);
+        float speed = rocketLaunch.CalculateSpeed(stage);
         
+        position += stageLaunchDirection * (speed * Time.deltaTime);
+        transform.position = position;
     }
     public void Stage2End(StageModel stage)
     {
-        
+        currentStage3Speed = rocketLaunch.CalculateSpeed(stage);
     }
+
+
     
+    private float currentStage3Speed;
+    private float thrustDecayRate;
     
     public void Stage3Start(StageModel stage)
     {
+        float relativeMassFactor = stage.mass / stage.referenceStageMass; 
+        thrustDecayRate = (stage.referenceStageMass / currentStage3Speed) * relativeMassFactor;
         
+        Debug.Log("Thrust decay rate: " + thrustDecayRate);
+        Debug.Log("Current stage 3 speed: " + currentStage3Speed);
     }
     public void Stage3Update(StageModel stage)
     {
+        DecreaseSpeed(stage);
+        Debug.Log("Current stage 3 speed: " + currentStage3Speed);
+        Vector2 stageLaunchDirection = rocketLaunch.GetFlightDirection(stage);
         
+        // TODO -- Additive gravity factor for more realistic descent
+        position += stageLaunchDirection * (currentStage3Speed * Time.deltaTime) + Vector2.down * (rocketLaunch.gravityStrength * Time.deltaTime);
+        transform.position = position;
     }
     public void Stage3End(StageModel stage)
-    {
-        
+    { 
+
     }
     
-    
-    
+    // The speed which stage 3 should initially have if expected to reach 0 in reference time
+    public float referenceStage3Speed = 5.5f;  
+    // The time in seconds for referenceSpeed to reach 0
+    public float referenceTime = 5f;    
+    private void DecreaseSpeed(StageModel stage)
+    {
+        float referenceDecrementPerSecond = referenceStage3Speed / referenceTime;
+        float adjustedDecrementPerSecond = referenceDecrementPerSecond * (stage.referenceStageMass / stage.mass);
+
+        float speedDecrementPerFrame = adjustedDecrementPerSecond * Time.deltaTime;
+
+        currentStage3Speed -= speedDecrementPerFrame;
+        currentStage3Speed = Mathf.Max(currentStage3Speed, 0f);
+    }
 }
