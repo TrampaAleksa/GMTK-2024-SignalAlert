eeede52320d298dea06199dcf6303a2cbf5e7b9c
Aleksa Trampa <alektramp@gmail.com>
2024-08-18 02:51:50 +0200

    Fixed Line renderer utility to finally work with Simulation

--- Assets/Aleksa/Scripts/LineRendererUtility.cs
+++ Assets/Aleksa/Scripts/LineRendererUtility.cs
@@ -9,35 +9,59 @@ public class LineRendererUtility : MonoBehaviour
 
     private List<Vector3> positions = new List<Vector3>();
     
-    private float timescale = 1f;
-
-    public float numberOfSeconds;
+    public float flightDuration = 15f;
     private float currentTime = 9000f;
+
+    public bool simulateInRealTime = false;
 
     [ContextMenu("Simulate Flight")]
     public void SimulateFlight()
     {
-        currentTime = 0f;
-        
         RocketLaunch.Init();
-        
+        RocketLaunch.transform.position = Vector3.zero;
+
         positions.Clear();
         lineRenderer.positionCount = 0;
         positions.Add(Vector2.zero);
+        
+        flightDuration = RocketLaunch.GetTotalDuration();
+        
+        if (simulateInRealTime)
+        {
+            currentTime = 0f;
+            RocketLaunch.Launch();
+            return;
+        }
+        
 
-        Time.timeScale = timescale;
-        RocketLaunch.Launch();
+        RocketLaunch.stage1.OnStageUpdate += (StageModel model) => positions.Add(RocketLaunch.rocketStageEvents.position);
+        RocketLaunch.stage2.OnStageUpdate += (StageModel model) => positions.Add(RocketLaunch.rocketStageEvents.position);
+        RocketLaunch.stage3.OnStageUpdate += (StageModel model) => positions.Add(RocketLaunch.rocketStageEvents.position);
+        RocketLaunch.stage3.OnStageEnd += (StageModel model) => SetPositions();
+        
+        RocketLaunch.rocketStateMachine.LaunchStateMachine();
+        
+        var numberOfUpdates = (int) (flightDuration * (1f / Time.fixedDeltaTime));
+        
+        for (int i = 0; i < numberOfUpdates; i++) // instant simulation of flight
+            RocketLaunch.rocketStateMachine.UpdateTime(Time.fixedDeltaTime);
     }
 
     private void FixedUpdate()
     {
         currentTime += Time.fixedDeltaTime;
         
-        if (currentTime < numberOfSeconds)
+        if (currentTime < flightDuration)
         {
             positions.Add(RocketLaunch.transform.position);
             lineRenderer.positionCount = positions.Count;
             lineRenderer.SetPositions(positions.ToArray());
         }
+    }
+
+    private void SetPositions()
+    {
+        lineRenderer.positionCount = positions.Count;
+        lineRenderer.SetPositions(positions.ToArray());
     }
 }
